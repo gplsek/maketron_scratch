a49e5405507d35bca7d6e615f540839a3d950926
George Plsek <george@gtekg.com>
2013-03-18 14:46:50 -0700

    update`:wq

---
+++ sites/all/themes/scratcher/includes/scripts.js
@@ -0,0 +1,225 @@
+/***********************************
+*    Project scripts
+*    Developer: tragic{media}
+***********************************/
+
+var $ = jQuery.noConflict(),
+
+/* HTML5 override for < IE9 */
+earlyIE = function() {
+    document.createElement('header');
+    document.createElement('nav');
+    document.createElement('section');
+    document.createElement('article');
+    document.createElement('aside');
+    document.createElement('footer');
+    document.createElement('hgroup');
+},
+
+/* check for html5 placeholder support */
+phSupport = function() {
+    var i = document.createElement('input');
+    return 'placeholder' in i;
+}, /* if(phSupport() == true){} */
+
+/* resetFields - removes form input value on mouse click, returns value on blue */
+resetFields = function() {
+    var i, j, l, label, len, nodes,
+    inputArray = $('input.form-text');
+
+    for(i=0, len = inputArray.length; i<len; i++){
+        if(inputArray[i].value != '') {
+            if(phSupport() == true) {
+                inputArray[i].setAttribute('placeholder', inputArray[i].value);
+            } else {
+                inputArray[i].setAttribute('value', inputArray[i].value);
+            }
+        } else {
+            nodes = inputArray[i].parentNode.childNodes;
+            for (j = 0, l = nodes.length; j < l; j++) {
+                if(nodes[j].tagName == 'LABEL') label = nodes[j].innerHTML;
+            }
+            label = label.substr(0, label.indexOf('<'));
+            if(phSupport() == true) {
+                inputArray[i].setAttribute('placeholder', label);
+            } else {
+                inputArray[i].setAttribute('value', label);
+            }
+        }
+        console.log(inputArray[i].placeholder);
+        if(inputArray[i].type == 'text') {
+            inputArray[i].onfocus = function() {
+                if(this.value == this.defaultValue) this.value = '';
+            }
+            inputArray[i].onblur = function() {
+                if(this.value == '') this.value = this.defaultValue;
+            }
+        }
+    }
+},
+
+
+/* add target=_blank to external links */
+externalLinks = function() {
+    var i, len,
+    links = document.getElementsByTagName('a'),
+    urlPaths = new Array('.pdf','.doc','.docx','.mov','.avi','.mpg','.mp3','.com','.net','.biz','.us','.org','.ca','.gov','.co/','.info','.ws','.ca', '.me', '.mobi', '.mx', '.tv');
+
+    for(i = 0, len = links.length; i < len; i++) {
+        for(var x = 0;x<urlPaths.length;x++) {
+            if((links[i].href.indexOf(window.location.host) == -1) && (links[i].href != null && links[i].href != '' && links[i].href != '#') && (links[i].href.indexOf(urlPaths[x]) != -1)) {
+                if(links[i].className.indexOf('external') == -1) {
+                    links[i].className = (links[i].className == '') ? 'external' : links[i].className + ' external';
+                    links[i].target = '_blank';
+                }
+                break;
+            }
+        }
+    }
+},
+
+/* contactDefaults - sets default values for form inputs */
+contactDefaults = function(formId) {
+    var i, prev, len,
+    inputs = $('#' + formId + ' input.form-text');
+
+    for(i = 0, len=inputs.length; i < len; i++) {
+        prev = inputs[i].previousSibling;
+        while(prev.nodeType != 1) {
+            prev = prev.previousSibling;
+        }
+        inputs[i].defaultValue = prev.innerHTML.slice(prev.innerHTML, prev.innerHTML.indexOf(' ')) + '*';
+        prev.style.display = 'none';
+    }
+},
+
+/* validateForm - Handles client-side form validation */
+validateForm = function (form) {
+    var i, len, inputArray, selectArray, thisInput, defVal, regmatch, newregex, errorsArray;
+
+    if(typeof(form) != Object) form = $('#' + form);
+    inputArray = form.find('input.form-text');
+    selectArray = form.find('select.form-select');
+    $('#' + form[0].id + ' #edit-submit').click(function() {
+        errorsArray=[];
+        for(i = 0, len = inputArray.length; i < len; i++){
+            thisInput = inputArray[i];
+            defVal = thisInput.defaultValue;
+            if(thisInput.value == '' || thisInput.value == 'NULL' || thisInput.value == defVal) {
+                $(thisInput).addClass('error');
+                errorsArray.push(i);
+            }
+        }
+        for(i = 0, i < selectArray.length; i < len; i++) {
+            thisInput = selectArray[i];
+            defVal = thisInput.options[thisInput.selectedIndex].text;
+            if(thisInput.value == defVal || thisInput.value == '' || thisInput.value == 'NULL') {
+                $(thisInput).addClass('error');
+                errorsArray.push(i);
+            }
+        }
+        if(errorsArray.length > 0) {
+            return false;
+        }
+    });
+},
+
+reNod = function() {
+    $.ajax({
+        type:'GET',
+        dataType: 'html',
+        url: '?q=clp',
+        success:function(data){
+            var html = $('<div>').html(data);
+            var item = html.find('#sQue').html();
+            doc = '?q=node/' + item;
+            getWinLose(doc);
+        }
+    });
+},
+
+getWinLose = function(doc) {
+    _canvas = $('#scratch-canvas');
+    _ajax = $('#tempAjax');
+    $.ajax({
+        type: 'GET',
+        dataType:'html',
+        url: doc,
+        success:function(data) {
+            sml = $('<div>').html(data);
+            var contents = sml.find('#main').html();
+            _ajax.html(contents);
+            _canvas.addClass('finished');
+            _ajax.addClass('finished');
+            _canvas.wScratchPad('clear');
+            _canvas.remove();
+            _ajax.removeClass('scratch-box');
+            _ajax.css('height', 'auto'); /*tmp*/
+        }
+    });
+}
+
+
+$(document).ready(function() {
+    /* default */
+    resetFields();
+    externalLinks();
+    if($('#webform-client-form').length) {
+        contactDefaults('webform-client-form');
+        validateForm('webform-client-form')
+    }
+    if($('#content table').length) $('#content table').wrap('<div class="table-wrapper">');
+
+     /* iphone > 5.0 swipe */
+    var Canvas = document.getElementById('scratch-canvas');
+    if(Canvas) {
+        Canvas.ontouchstart = function(e){
+            e.preventDefault();
+        };
+    }
+    var Cans = 0;
+    if($('.scratch-block').length) {
+        var imgUnder, imgOver, h = window.location.host + '/?q=';
+        if(Cans < 1) {
+
+        imgOver = $('#imgTop img').attr('src');
+        imgUnder = $('#imgBot img').attr('src');
+
+        $('#scratch-canvas').wScratchPad({
+            width         : 680,                 // set width - best to match image width
+            height        : 450,                 // set height - best to match image height
+            image         : imgUnder,
+            image2        : imgOver,
+            overlay       : 'none',
+            size          : 20,
+            scratchDown   : null,
+            scratchUp     : null,
+            scratchMove   : null,
+            cursor        : 'sites/all/themes/scratcher/images/cursor.png',
+            scratchDown: function(e, percent){},
+            scratchUp: function(e, percent){},
+            scratchMove: function(e, percent) {
+                if(percent > 5) {
+                    if(Cans == 0) {
+                        Cans = 1;
+                        reNod();
+                    }
+                }
+            },
+        });
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*end*/
---
+++ sites/all/themes/scratcher/template.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * Return a themed breadcrumb trail.
+ *
+ * @param $breadcrumb
+ *   An array containing the breadcrumb links.
+ * @return a string containing the breadcrumb output.
+ */
+function scratcher_breadcrumb($vars) {
+  $breadcrumb = $vars['breadcrumb'];
+  $last = null;
+
+  if(!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to screen-reader users. Make the heading invisible with .element-invisible.
+    if(count($breadcrumb) > 1) $last = array_pop($breadcrumb);
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+    $output .= '<div class="breadcrumb">' . implode('<span class="arrow">></span>', $breadcrumb);
+    if($last) $output .= '<span class="arrow">></span><span class="current">'.$last.'</span>';
+    $output .= '</div>';
+    return $output;
+  }
+}
+
+/**
+ * Override or insert variables into the maintenance page template.
+ */
+function scratcher_preprocess_maintenance_page(&$vars) {
+  // While markup for normal pages is split into page.tpl.php and html.tpl.php,
+  // the markup for the maintenance page is all in the single
+  // maintenance-page.tpl.php template. So, to have what's done in
+  // scratcher_preprocess_html() also happen on the maintenance page, it has to be
+  // called here.
+  scratcher_preprocess_html($vars);
+}
+
+/**
+ * Override or insert variables into the html template.
+ */
+function scratcher_preprocess_html(&$vars) {
+    // Classes for body element. Allows advanced theming based on context
+    // (home page, node of certain type, etc.)
+    if (!$vars['is_front']) {
+      // Add unique class for each page.
+      $path = drupal_get_path_alias($_GET['q']);
+      // Add unique class for each website page
+      $section = (strpos($path, '/')) ? substr($path, 0, strpos($path, '/')) : $path;
+      $page = preg_split('/\//', $path);
+      $page = $page[count($page) - 1];
+      if (arg(0) == 'node') {
+          if (arg(1) == 'add') {
+              $page = 'node-add';
+          } elseif (is_numeric(arg(1)) && (arg(2) == 'edit' || arg(2) == 'delete')) {
+              $page = 'node-' . arg(2);
+          }
+      }
+      $vars['classes_array'][] = drupal_html_class('path-' . $page);
+      $vars['classes_array'][] = drupal_html_class('section-' . $section);
+
+    }
+}
+
+/**
+ * Override or insert variables into the page template.
+ */
+function scratcher_preprocess_page(&$vars) {
+  // Prepare header.
+  $site_fields = array();
+  if (!empty($vars['site_name'])) {
+    $site_fields[] = $vars['site_name'];
+  }
+  if (!empty($vars['site_slogan'])) {
+    $site_fields[] = $vars['site_slogan'];
+  }
+  $vars['site_title'] = implode(' ', $site_fields);
+  if (!empty($site_fields)) {
+    $site_fields[0] = '<span>' . $site_fields[0] . '</span>';
+  }
+  $vars['site_html'] = implode(' ', $site_fields);
+
+  // Set a variable for the site name title and logo alt attributes text.
+  $slogan_text = $vars['site_slogan'];
+  $site_name_text = $vars['site_name'];
+  $vars['site_name_and_slogan'] = $site_name_text . ' ' . $slogan_text;
+
+  // Set ajax and page-node tpls
+  if(isset($_GET['ajax']) && $_GET['ajax'] == 1) {
+    $vars['theme_hook_suggestions'] = 'page__ajax';
+  }
+  if(!empty($vars['node'])) {
+    if($vars['node']->type != '') {
+      $vars['theme_hook_suggestions'][] = 'page__'.str_replace('_', '--', $vars['node']->type);
+    }
+    if(strtolower($vars['node']->title) == 'contact') {
+      $vars['theme_hook_suggestions'][] = 'page__contact';
+    }
+    if(strtolower($vars['node']->title) == 'gallery') {
+      $vars['theme_hook_suggestions'][] = 'page__gallery';
+	}
+  }
+
+  // Initialize section image if it exists.
+  if (!empty($vars['node'])) {
+    $items = field_get_items('node', $vars['node'], 'field_section_image');
+    if (!empty($items)) {
+      $vars['section_image'] = theme('image_style', array('style_name' => 'banner', 'path' => $items[0]['uri']));
+    }
+  }
+}
+
+/**
+ * Override or insert variables into the node template.
+ */
+function scratcher_preprocess_node(&$vars) {
+  $vars['submitted'] = $vars['date'] . ' � ' . $vars['name'];
+}
+
+/**
+ * Override or insert variables into the comment template.
+ */
+function scratcher_preprocess_comment(&$vars) {
+    $vars['submitted'] = $vars['author'].'<span class="pipe">|</span><span class="date">'.$vars['created'].'</span>';
+    if($vars['id']%2 == 0) $vars['comment_stripe'] = 'comment-even';
+    else $vars['comment_stripe'] = 'comment-odd';
+}
+
+/*
+* Override filter.module's theme_filter_tips() function to disable tips display.
+*/
+function scratcher_form_comment_form_alter(&$form, &$form_state, $form_id) {
+  $form['comment_body']['#after_build'][] = 'remove_tips';
+}
+
+function remove_tips(&$form) {
+  unset($form['und'][0]['format']['guidelines']);
+  unset($form['und'][0]['format']['help']);
+  return $form;
+}
+
+/**
+* Add unique class (mlid) to all menu items.
+* with Menu title as class
+*/
+function scratcher_menu_link(&$vars) {
+  $element = $vars['element'];
+  $sub_menu = '';
+  $name_id = strtolower(strip_tags($element['#title']));
+// remove colons and anything past colons
+  if (strpos($name_id, ':')) $name_id = substr ($name_id, 0, strpos($name_id, ':'));
+//Preserve alphanumerics, everything else goes away
+  $pattern = '/[^a-z]+/ ';
+  $name_id = preg_replace($pattern, '', $name_id);
+  $element['#attributes']['class'][] = 'menu-' . $name_id;
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
